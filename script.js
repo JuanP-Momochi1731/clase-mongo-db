0e6a1469f2fbb7e0e11e39275fa2fc4db2c9beed
JuanP-Momochi1731 <145062278+JuanP-Momochi1731@users.noreply.github.com>
2023-09-27 20:00:41 -0500

    27/09/2023

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+function countAllStars(galaxies) {
+  var totalStars = galaxies.reduce(function (accumulator, stars) {
+    return accumulator + stars;
+  }, 0); // El segundo argumento (0) es el valor inicial del acumulador.
+
+  return totalStars;
+}
+
+const galaxies = [10, 3, 2, 1, 6, 1, 9, 2];
+console.log(countAllStars(galaxies)); 
+// Mostrará el total de estrellas en la consola
 
